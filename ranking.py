ac145dbdf5cff7b32feade12797b86359b4a2ea0
nguyennhuhha <nhuha3110@gmail.com>
2023-12-24 21:22:32 +0700

    evaluating

---
+++ ranking.py
@@ -0,0 +1,103 @@
+import os
+import time
+import pathlib
+from PIL import Image
+from argparse import ArgumentParser
+
+import faiss
+
+from src.feature_extraction import MyResnet50, MyVGG16, MyXception, MyEfficient
+# from src.dataloader import get_transformation
+
+ACCEPTED_IMAGE_EXTS = ['.jpg', '.png']
+
+
+def get_image_list(image_root):
+    image_root = pathlib.Path(image_root)
+    image_list = list()
+    for image_path in os.listdir(image_root):
+        image_list.append(image_path[:-4])
+    image_list = sorted(image_list, key = lambda x: x)
+    return image_list
+
+def main():
+
+    parser = ArgumentParser()
+    parser.add_argument("--feature_extractor", required=True, type=str, default='Resnet50')
+    parser.add_argument("--dataset", required=True, type=str, default='paris')
+    # parser.add_argument("--device", required=False, type=str, default='cuda:0')
+    parser.add_argument("--top_k", required=False, type=int, default=11)
+    parser.add_argument("--crop", required=False, type=bool, default=False)
+
+    print('Ranking .......')
+    start = time.time()
+
+    args = parser.parse_args()
+    
+    if (args.dataset == 'oxford'):
+        image_root = './dataset/oxford'
+        query_root = './dataset/groundtruth/oxford'
+        evaluate_root = './dataset/evaluation/oxford'
+        feature_root = './dataset/feature/oxford'
+    else:
+        image_root = './dataset/paris'
+        query_root = './dataset/groundtruth/paris'
+        evaluate_root = './dataset/evaluation/paris'
+        feature_root = './dataset/feature/paris'
+
+    # Load module feature extraction 
+    if (args.feature_extractor == 'Resnet50'):
+        extractor = MyResnet50()
+    elif (args.feature_extractor == 'VGG16'):
+        extractor = MyVGG16()
+    elif (args.feature_extractor == 'Xception'):
+        extractor = MyXception()
+    elif (args.feature_extractor == 'Efficient'):
+        extractor = MyEfficient()
+    else:
+        print("No matching model found")
+        return
+
+    img_list = get_image_list(image_root)
+    # transform = get_transformation()
+
+    for path_file in os.listdir(query_root):
+        if (path_file[-9:-4] == 'query'):
+            rank_list = []
+
+            with open(query_root + '/' + path_file, "r") as file:
+                img_query, left, top, right, bottom = file.read().split()
+
+            if(args.dataset == 'oxford'):
+                img_query = img_query[5:]
+
+            test_image_path = pathlib.Path(image_root + '/' + img_query + '.jpg')
+            pil_image = Image.open(test_image_path)
+            pil_image = pil_image.convert('RGB')
+
+            path_crop = 'original'
+            if (args.crop):
+                pil_image=pil_image.crop((float(left), float(top), float(right), float(bottom)))
+                path_crop = 'crop'
+
+            # image_tensor = transform(pil_image)
+            # image_tensor = image_tensor.unsqueeze(0).cpu()
+
+            feat = extractor.extract_features1(pil_image)
+
+
+            indexer = faiss.read_index(feature_root + '/' + args.feature_extractor + '.index.bin')
+
+            _, indices = indexer.search(feat, k=args.top_k)  
+
+            for index in indices[0]:
+                rank_list.append(str(img_list[index]))
+
+            with open(evaluate_root + '/' + path_crop + '/' + args.feature_extractor + '/' + path_file[:-10] + '.txt', "w") as file:
+                file.write("\n".join(rank_list))
+
+    end = time.time()
+    print('Finish in ' + str(end - start) + ' seconds')
+
+if __name__ == '__main__':
+    main()
---
+++ src/compute.py
@@ -0,0 +1,55 @@
+import os
+
+
+def compute_AP(pos_set, ranked_list):
+    relevant = 0.0
+    average_precision = 0.0
+    number_retrieve = 0
+
+    for item in ranked_list:
+        number_retrieve += 1
+        if item not in pos_set:
+            continue
+        
+        relevant += 1
+        average_precision += (relevant/number_retrieve)
+
+    if relevant == 0:
+        return 0
+    return average_precision / relevant
+
+def compute_mAP(feature_extractor, dataset, crop = False):
+
+    if (dataset == 'oxford'):
+        root_groundtruth = './dataset/groundtruth/oxford'
+        root_evaluation = './dataset/evaluation/oxford'
+    else:
+        root_groundtruth = './dataset/groundtruth/paris'
+        root_evaluation = './dataset/evaluation/paris'
+
+    if (crop):
+        path_evaluation =  root_evaluation + '/crop'
+    else:
+        path_evaluation = root_evaluation + '/original'
+
+    path_evaluation += ('/' + feature_extractor)
+
+    AP = 0.0
+    number_query = 0.0
+
+    for query in os.listdir(path_evaluation):
+        with open(root_groundtruth + '/' + query[:-4] + '_good.txt', 'r') as file:
+            good_set = file.read().split('\n')
+        with open(root_groundtruth + '/' + query[:-4] + '_ok.txt', 'r') as file:
+            ok_set = file.read().split('\n')
+            
+        # positive set of ground truth = ok_set + good_set
+        pos_set = ok_set + good_set
+
+        with open(path_evaluation + '/' + query) as file:
+            ranked_list = file.read().split('\n')
+        
+        AP += compute_AP(pos_set, ranked_list)
+        number_query += 1
+    
+    return AP / number_query
