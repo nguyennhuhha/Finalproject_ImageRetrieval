3817f8a570126a6b550d9f5451c8e35452c9145b
21522057 <21522057@gm.uit.edu.vn>
2023-12-27 09:01:01 +0700

    update streamlit

--- demo.py
+++ demo.py
@@ -48,57 +48,63 @@ def retrieve_image(img, data,feature_extractor,number_k):
     return indices[0], image_root
 
 def main():
-    st.title('IMAGE RETRIEVAL')
+    # st.title('IMAGE RETRIEVAL')
     
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.header('QUERY')
-
-        st.subheader('Choose dataset')
-        data = st.selectbox('', ( 'Paris', 'Oxford'))
-
-        st.subheader('Choose feature extractor')
-        option = st.selectbox('', ( 'Resnet50', 'VGG16', 'Xception', 'Efficient'))
+    with st.container():
+        #st.header('QUERY')
+        with st.container():
+            cola, colb = st.columns(2)
+            with cola:
+                st.subheader('Dataset')
+                data = st.selectbox('', ( 'Paris', 'Oxford'))
+            with colb:
+                st.subheader('Model')
+                option = st.selectbox('', ( 'Resnet50', 'VGG16', 'Xception', 'Efficient'))
 
         st.subheader('Upload image')
         img_file = st.file_uploader(label='', type=['png', 'jpg'])
-
-        if img_file:
-            img = Image.open(img_file)
-            # Get a cropped image from the frontend
-            cropped_img = st_cropper(img, realtime_update=True, box_color='#FF0004')
-            
-            # Manipulate cropped image at will
-            st.write("Preview")
-            _ = cropped_img.thumbnail((150,150))
-            st.image(cropped_img)
+        colc, cold = st.columns(2)
+        with colc:
+            if img_file:
+                img = Image.open(img_file)
+                # Get a cropped image from the frontend
+                cropped_img = st_cropper(img, realtime_update=True, box_color='#FF0004')
+                # Manipulate cropped image at will
+                with cold:
+                    st.write("Preview")
+                    _ = cropped_img.thumbnail((300,300))
+                    st.image(cropped_img)
         
         with st.form("query"):
             number_k = st.text_input("Number of outcomes: ")
             submitted = st.form_submit_button("Submit")
-
-    with col2:
+    
+    with st.container():
         st.header('RESULT')
+        col1, col2, col3 = st.columns(3)
         if submitted:
             if img_file:
-                st.markdown('**Retrieving .......**')
+                text_placeholder=st.markdown('**Retrieving .......**')
                 start = time.time()
 
                 retriev, image_root = retrieve_image(cropped_img, data, option, int(number_k))
                 image_list = get_image_list(image_root)
 
                 end = time.time()
-                st.markdown('**Finish in ' + str(end - start) + ' seconds**')
+                for i in range(0, int(number_k), 1):
+                    print(image_list[retriev[i]])
+                with col1: 
+                    st.write("Preview")
+                    st.image(cropped_img)
+                    st.markdown('**Finish in ' + str(end - start) + ' seconds**')
+                    text_placeholder.empty()
 
-                col3, col4 = st.columns(2)
-
-                with col3:
+                with col2:
                     for i in range(0,int(number_k), 2):
                         image = Image.open(image_list[retriev[i]])
                         st.image(image, use_column_width = 'always')
 
-                with col4:
+                with col3:
                     for i in range(1,int(number_k), 2):
                         image = Image.open(image_list[retriev[i]])
                         st.image(image, use_column_width = 'always')
